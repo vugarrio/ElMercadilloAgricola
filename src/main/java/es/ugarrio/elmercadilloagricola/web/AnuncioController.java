944aa363ab23ad0027b98c7d29f64d3f20d06ecc
Vicente Ugarrio (Acer) <vugarrio@gmail.com>
2015-11-03 16:44:34 +0100

    Actualización 20151103

---
+++ src/main/java/es/ugarrio/elmercadilloagricola/web/AnuncioController.java
@@ -0,0 +1,20 @@
+package es.ugarrio.elmercadilloagricola.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value="/anuncios")
+public class AnuncioController {
+	
+	@Autowired
+	//private AnuncioService AnuncioService;
+	
+	/** Logger for this class and subclasses */
+    protected final Log logger = LogFactory.getLog(getClass());
+
+}
